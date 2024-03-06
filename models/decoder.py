73ee9fe014cbc871bbaed40b0ccfd7a96f6621b8
Ahmed Hattali <HattaliAhmed@gmail.com>
2024-03-06 18:16:22 +0100

    Update decoder.py

--- models/decoder.py
+++ models/decoder.py
@@ -1,21 +1,10 @@
-import torch.nn as nn 
+from torch import nn
+class Decoder(nn.Module):
+    def __init__(self, hidden_dim, output_dim):
+        super(Decoder, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.lin = nn.Linear(hidden_dim, output_dim)
 
-class BaseEdgeDecoder(nn.Module):
-  def __init__(self, hidden_dim):
-    super().__init__()
-    self.hidden_dim = hidden_dim
-    self.source_lin = nn.Linear(hidden_dim, hidden_dim)
-    self.target_lin = nn.Linear(hidden_dim, hidden_dim)
-  
-  def forward(self, hiddens, edge_index):
-    zs = self.source_lin(hiddens) # N x H
-    zt = self.target_lin(hiddens) # N x H
-    return (zs[edge_index[0]] * zt[edge_index[1]]).sum(dim=-1) # M x 1
-      
-class EdgeMaskDecoder(BaseEdgeDecoder):
-  def __init__(self, hidden_dim):
-    super().__init__(hidden_dim)
-
-  def forward(self, hiddens, edge_index):
-    out = super().forward(hiddens, edge_index).sigmoid().squeeze(-1)
-    return out
+    def forward(self, x):
+        return self.lin(x)
